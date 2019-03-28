cc965135d3aca0190cce1e794009360fada4b85e
tanduong <tanduong@live.com>
2019-03-28 00:19:04 -0500

    add client

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import AppNavBar from "./components/layout/AppNavBar";
 import Dashboard from "./components/layout/Dashboard";
+import AddClient from './components/clients/AddClient';
+// import EditClient from './components/clients/EditClient';
+// import ClientDetails from './components/clients/ClientDetails';
+// import Login from './components/auth/Login';
+// import Register from './components/auth/Register';
+// import Settings from './components/settings/Settings';
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -18,6 +24,11 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path={"/"} component={Dashboard} />
+                <Route
+                  exact
+                  path="/client/add"
+                  component={AddClient}
+                />
               </Switch>
             </div>
           </div>
